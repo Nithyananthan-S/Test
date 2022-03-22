600ffe182df2ccdcaf105d075f931b9bcf97225a
Nithyananthan-S <nithibit@hotmail.com>
2022-03-22 14:04:50 -0400

    Test Benchmarks

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using Test;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Test");
+var s = "test2";
+Console.WriteLine($"selected {s}"); 
+Console.ReadLine();
+BenchmarkRunner.Run<BenchmarkStringIntialization>();
+Console.ReadLine();
